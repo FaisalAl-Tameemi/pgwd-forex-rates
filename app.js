07f5da8f4a9cd28e5849f09e2d564615c3f9a1df
Faisal Al-Tameemi <faisal.al.tameemi@gmail.com>
2017-02-06 01:16:16 -0500

    removed solutions

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
+const axios = require('axios');
 const ratesUtil = require('./lib/ratesUtil');
 
-/**
- * Example of calling the `getRates` method
- */
-ratesUtil
-  .getRates('CAD')
+// Example of using axios to make a GET request
+axios
+  .get('http://api.fixer.io/latest')
   .then((response) => {
-    console.log(response.data.rates);
+    console.log('Latest Exchange Rates:');
+    console.log(response.data);
   })
   .catch(console.error);
 
+// Example of calling the `getRates` method
 /**
- * Example of calling the `getRatesCompare` method
+
+ratesUtil
+ .getRates('CAD')
+ .then((response) => {
+   console.log(response.data.rates);
+ })
+ .catch(console.error);
+
  */
+
+// Example of calling the `getRatesCompare` method
+/**
 ratesUtil
   .getRatesCompare(['CAD', 'USD'], '2015-10-10')
   .then((response) => {
     console.log(response.data.rates);
   })
   .catch(console.error);
+
+ */
